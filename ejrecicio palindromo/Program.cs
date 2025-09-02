e08d8cb1b5d6edd377e9ba7df9b6af228f9f3dbd
cristobal <cristobal@DESKTOP-C6EVRJF>
2025-09-01 20:17:33 -0600

    Agregar archivos de proyecto.

---
+++ ejrecicio palindromo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe una palabra o frase, reswivaremos si es un palindromo!");
+string? input = Console.ReadLine();
+bool isPalindrome = IsPalindrome(input);
+Console.ReadLine(); = input?.ToLower().Replace(" ", "").Replace(",", "").Replace(".", "").Replace("!", "").Replace("?", "") ?? "";
+if (isPalindrome)
+{
+    Console.WriteLine("Es un palindromo");
+}
+else
+{
+    Console.WriteLine("No es un palindromo");
+}
+Console.WriteLine("Presiona cualquier tecla para salir...");
+Console.ReadKey();
